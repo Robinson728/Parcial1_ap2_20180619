6ed701cf9e32ee2a96ecd116b2a4a46cb5b18b20
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-02 18:25:11 -0400

    Creando clase Productos.

---
+++ Models/Productos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_ap2_20180619.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        
+        [Required(ErrorMessage = "Llenar campo por favor.")]
+        public string Descripcion { get; set; }
+
+        [Range(1, 100000, ErrorMessage = "El valor debe ser mayor a cero.")]
+        public int Existencia { get; set; }
+
+        [Range(1, 10000000, ErrorMessage = "El valor debe ser mayor a cero.")]
+        public double Costo { get; set; }
+
+        public double ValorInventario { get; set; }
+    }
+}
