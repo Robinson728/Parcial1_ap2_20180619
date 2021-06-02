5e2934af6c0b01a8bfb1554515bae3cbccd6df20
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-02 18:28:53 -0400

    Agregando clase Productos al Contexto.

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Parcial1_ap2_20180619.Models;
 
 namespace Parcial1_ap2_20180619.DAL
 {
     public class Contexto : DbContext
     {
+        public DbSet<Productos> Productos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data Source = Data\GestionProductos.db");
-
         }
     }
 }
