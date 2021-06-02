07fe51aebd59a76003d2d86a6b9e4733ad45215c
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-02 11:20:00 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Parcial1_ap2_20180619.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\GestionProductos.db");
+        }
+    }
+}
